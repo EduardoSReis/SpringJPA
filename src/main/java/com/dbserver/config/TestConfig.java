c782b088892be1d0876e033235f30b58be56084f
eduardo.reis <eduardo.reis@dbserver.com.br>
2019-10-07 14:23:41 -0300

    Payment

--- src/main/java/com/dbserver/config/TestConfig.java
+++ src/main/java/com/dbserver/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.dbserver.entities.Category;
 import com.dbserver.entities.Order;
 import com.dbserver.entities.OrderItem;
+import com.dbserver.entities.Payment;
 import com.dbserver.entities.Product;
 import com.dbserver.entities.User;
 import com.dbserver.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
